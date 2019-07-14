9efdc3423e0208019a6894f030e98ec2d65b9e2c
wuzuqiang <1045774869@qq.com>
2019-07-15 07:35:37 +0800

    00

--- DebatchGenerateCode/ClassLibrary1/Main.cs
+++ DebatchGenerateCode/ClassLibrary1/Main.cs
@@ -42,14 +42,118 @@ namespace ClassLibrary1
             listMember.Add(new MemberFromInfo("CreateTime", "DateTime"));
             listMember.Add(new MemberFromInfo("UpdateTime", "DateTime"));
             listMember.Add(new MemberFromInfo("RowVersion", "byte[]"));
-            string file1 = AppDomain.CurrentDomain.BaseDirectory + string.Format(@"Fussion\src\Application\Fusion.Context.BasicInfo.Application\Models\Product\"+"{0}s\\{0}Model.cs"
+            string content;
+            #region 
+            string file = AppDomain.CurrentDomain.BaseDirectory + string.Format(@"Fussion\src\Application\Fusion.Context.BasicInfo.Application\Models\Product\" + "{0}s\\{0}Model.cs"
                 , cN);
-            string content = (new ClassApplication()).GetApplicationModel(listMember).Replace("_左括号_", "{").Replace("_右括号_", "}");
-            (new FileUtils()).writeToFile(file1, content);
-            System.Diagnostics.Process.Start(file1);
+            content = (new ClassApplication()).GetApplicationModel(listMember).Replace("_左括号_", "{").Replace("_右括号_", "}");
+            (new FileUtils()).writeToFile(file, content);
+            System.Diagnostics.Process.Start(file);
+            #endregion
             string fileAppStore = AppDomain.CurrentDomain.BaseDirectory 
                 + string.Format(@"E:\useful\Thkj-Resource\origin\Fussion\src\Application\Fusion.Context.BasicInfo.Application\Services\Interfaces\Product\"
                 + "I{0}Service.cs", cN);
+
+            #region 
+            file = AppDomain.CurrentDomain.BaseDirectory + string.Format(
+                @"	Fussion\src\Application\Fusion.Context.BasicInfo.Application\Models\Product\_左括号_0_右括号_s\_左括号_0_右括号_Model.cs	", cN);
+            content = (new ClassApplication()).GetApplicationModel(listMember).Replace("_左括号_", "{").Replace("_右括号_", "}");
+            (new FileUtils()).writeToFile(file, content);
+            System.Diagnostics.Process.Start(file);
+            #endregion
+            #region 
+            file = AppDomain.CurrentDomain.BaseDirectory + string.Format(
+                @"	Fussion\src\Application\Fusion.Context.BasicInfo.Application\Services\Interfaces\Product\I_左括号_0_右括号_Service.cs	", cN);
+            content = (new ClassApplication()).GetApplicationModel(listMember).Replace("_左括号_", "{").Replace("_右括号_", "}");
+            (new FileUtils()).writeToFile(file, content);
+            System.Diagnostics.Process.Start(file);
+            #endregion
+            #region 
+            file = AppDomain.CurrentDomain.BaseDirectory + string.Format(
+                @"	Fussion\src\Application\Fusion.Context.BasicInfo.Application\Services\Product\_左括号_0_右括号_Service.cs	", cN);
+            content = (new ClassApplication()).GetApplicationModel(listMember).Replace("_左括号_", "{").Replace("_右括号_", "}");
+            (new FileUtils()).writeToFile(file, content);
+            System.Diagnostics.Process.Start(file);
+            #region 
+            #endregion
+            file = AppDomain.CurrentDomain.BaseDirectory + string.Format(
+                @"	Fussion\src\Domain\Fusion.Context.BasicInfo.Domain\Models\Product\_左括号_0_右括号_s\_左括号_0_右括号_.cs	", cN);
+            content = (new ClassApplication()).GetApplicationModel(listMember).Replace("_左括号_", "{").Replace("_右括号_", "}");
+            (new FileUtils()).writeToFile(file, content);
+            System.Diagnostics.Process.Start(file);
+            #region 
+            #endregion
+            file = AppDomain.CurrentDomain.BaseDirectory + string.Format(
+                @"	Fussion\src\Domain\Fusion.Context.BasicInfo.Domain\Repositories\Product\I_左括号_0_右括号_Repository.cs	", cN);
+            content = (new ClassApplication()).GetApplicationModel(listMember).Replace("_左括号_", "{").Replace("_右括号_", "}");
+            (new FileUtils()).writeToFile(file, content);
+            System.Diagnostics.Process.Start(file);
+            #endregion
+            #region 
+            file = AppDomain.CurrentDomain.BaseDirectory + string.Format(
+                @"	Fussion\src\Infrastructure\Fusion.Infrastructure.Mapping.BasicInfo\Models\Product\_左括号_0_右括号_s\_左括号_0_右括号_Map.cs	", cN);
+            content = (new ClassApplication()).GetApplicationModel(listMember).Replace("_左括号_", "{").Replace("_右括号_", "}");
+            (new FileUtils()).writeToFile(file, content);
+            System.Diagnostics.Process.Start(file);
+            #endregion
+            #region 
+            file = AppDomain.CurrentDomain.BaseDirectory + string.Format(
+                @"	Fussion\src\Infrastructure\Fusion.Infrastructure.Repository.BasicInfo\Repositories\Product\_左括号_0_右括号_Repository.cs	", cN);
+            content = (new ClassApplication()).GetApplicationModel(listMember).Replace("_左括号_", "{").Replace("_右括号_", "}");
+            (new FileUtils()).writeToFile(file, content);
+            System.Diagnostics.Process.Start(file);
+            #endregion
+            #region 
+            file = AppDomain.CurrentDomain.BaseDirectory + string.Format(
+                @"	Fussion\src\User Interface\Fusion\Extjs\Fusion\classic\src\BasicInfo\model\Product\_左括号_0_右括号_s\_左括号_0_右括号_Model.js	", cN);
+            content = (new ClassApplication()).GetApplicationModel(listMember).Replace("_左括号_", "{").Replace("_右括号_", "}");
+            (new FileUtils()).writeToFile(file, content);
+            System.Diagnostics.Process.Start(file);
+            #endregion
+            #region 
+            file = AppDomain.CurrentDomain.BaseDirectory + string.Format(
+                @"	Fussion\src\User Interface\Fusion\Extjs\Fusion\classic\src\BasicInfo\store\Product\_左括号_0_右括号_s\_左括号_0_右括号_Store.js	", cN);
+            content = (new ClassApplication()).GetApplicationModel(listMember).Replace("_左括号_", "{").Replace("_右括号_", "}");
+            (new FileUtils()).writeToFile(file, content);
+            System.Diagnostics.Process.Start(file);
+            #endregion
+            #region 
+            file = AppDomain.CurrentDomain.BaseDirectory + string.Format(
+                @"	Fussion\src\User Interface\Fusion\Extjs\Fusion\classic\src\BasicInfo\view\Product\_左括号_0_右括号_s\_左括号_0_右括号_.js	", cN);
+            content = (new ClassApplication()).GetApplicationModel(listMember).Replace("_左括号_", "{").Replace("_右括号_", "}");
+            (new FileUtils()).writeToFile(file, content);
+            System.Diagnostics.Process.Start(file);
+            #endregion
+            #region 
+            file = AppDomain.CurrentDomain.BaseDirectory + string.Format(
+                @"	Fussion\src\User Interface\Fusion\Extjs\Fusion\classic\src\BasicInfo\view\Product\_左括号_0_右括号_s\_左括号_0_右括号_Controller.js	", cN);
+            content = (new ClassApplication()).GetApplicationModel(listMember).Replace("_左括号_", "{").Replace("_右括号_", "}");
+            (new FileUtils()).writeToFile(file, content);
+            System.Diagnostics.Process.Start(file);
+            #endregion
+            #region 
+            file = AppDomain.CurrentDomain.BaseDirectory + string.Format(
+                @"	Fussion\src\User Interface\Fusion\Extjs\Fusion\classic\src\BasicInfo\view\Product\_左括号_0_右括号_s\_左括号_0_右括号_Toolbar.js	", cN);
+            content = (new ClassApplication()).GetApplicationModel(listMember).Replace("_左括号_", "{").Replace("_右括号_", "}");
+            (new FileUtils()).writeToFile(file, content);
+            System.Diagnostics.Process.Start(file);
+            #endregion
+            #region 
+            file = AppDomain.CurrentDomain.BaseDirectory + string.Format(
+                @"	Fussion\src\User Interface\Fusion\Extjs\Fusion\classic\src\BasicInfo\view\Product\_左括号_0_右括号_s\Find_左括号_0_右括号_.js	", cN);
+            content = (new ClassApplication()).GetApplicationModel(listMember).Replace("_左括号_", "{").Replace("_右括号_", "}");
+            (new FileUtils()).writeToFile(file, content);
+            System.Diagnostics.Process.Start(file);
+            #endregion
+            #region 
+            file = AppDomain.CurrentDomain.BaseDirectory + string.Format(
+                @"	Fussion\src\User Interface\Fusion\Extjs\Fusion\classic\src\BasicInfo\view\Product\_左括号_0_右括号_s\Modify_左括号_0_右括号_.js	", cN);
+            content = (new ClassApplication()).GetApplicationModel(listMember).Replace("_左括号_", "{").Replace("_右括号_", "}");
+            (new FileUtils()).writeToFile(file, content);
+            System.Diagnostics.Process.Start(file);
+            #endregion
+            #region 
+
         }
     }
 }
