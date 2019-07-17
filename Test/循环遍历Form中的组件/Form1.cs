2c9130068675ff991a6874bc19f0e9ee50a6c3d8
wuzuqiang <1045774869@qq.com>
2019-07-17 23:50:11 +0800

    循环遍历，最终获取Form中所有控件的基本信息

---
+++ Test/循环遍历Form中的组件/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 循环遍历Form中的组件
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //var aa = txtClassName
+            //var aa1 = label1.f
+            foreach (Control ctrl in this.Controls)
+            {
+            }
+            getCtrlControl(this);
+            richTextBox1.Text += sb.ToString();
+        }
+        StringBuilder sb = new StringBuilder();
+        public void getCtrlControl(Control ctrl)
+        {
+            if (ctrl is TableLayoutPanel || ctrl is System.Windows.Forms.Form)
+            {
+                foreach (Control ctrlOne in ctrl.Controls)
+                {
+                    getCtrlControl(ctrlOne);
+                }
+            }
+            else
+            {
+                sb.Append(string.Format("x:{0},y:{1},控件名称:{2}，width:{3},height:{4},font-size:{5},文本内容\n"
+                            , ctrl.Location.X, ctrl.Location.Y, ctrl.Name, ctrl.Width, ctrl.Height, ctrl.Font.Size, ctrl.Text));
+            }
+        }
+    }
+}
