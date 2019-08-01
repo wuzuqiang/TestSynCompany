37cf7fb46013a5bfc9c2ef8bd4ec610c78ddf452
wuzuqiang <zuqiang_wu@skyseaok.com>
2019-08-01 15:53:10 +0800

    AddDataToICollection

---
+++ Test/AddDataToICollection/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddDataToICollection
+{
+    public class ModelSet
+    {
+        public string DownloadBatch { get; private set; }
+        public string Remark { get; private set; }
+        public virtual ICollection<Model> OriginalOrderProductHistories { get; private set; } = new List<Model>();
+
+        public void AddModels(IList<Model> originalOrderProductHistories)
+        {
+            this.OriginalOrderProductHistories = originalOrderProductHistories;
+        }
+    }
+    public class Model
+    {
+        public string Name { get; set; }
+        public Model(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Test/AddDataToICollection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddDataToICollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Model> models = new List<Model>();
+            for(int i = 0; i < 3; i++)
+            {
+                models.Add(new Model(i.ToString()));
+            }
+            ModelSet modelSet = new ModelSet();
+            modelSet.AddModels(models);
+            List<Model> models1 = new List<Model>();
+            for (int i = 0; i < 2; i++)
+            {
+                models1.Add(new Model("B" + i.ToString()));
+            }
+            modelSet.AddModels(models1);
+
+            Console.ReadLine();
+        }
+    }
+}
