812a3440f553821bf8a2dca32eb0312ef1082f80
wuzuqiang <zuqiang_wu@skyseaok.com>
2019-09-26 10:58:18 +0800

    半天看不明白博客上写的代码，怀疑是多线程的原因，所以还是参考MSDN上的代码，这个肯定没错。

--- Test/ManualResetEventSlim_Test/Program.cs
+++ Test/ManualResetEventSlim_Test/Program.cs
@@ -1,49 +1,81 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-
-namespace ManualResetEventSlim_Test
+class MRESDemo
 {
 
-    class ProgramTest
+    static void Main()
     {
-        static void Main(string[] args)
+        MRES_SetWaitReset();
+        MRES_SpinCountWaitHandle();
+    }
+    // Demonstrates:
+    //      ManualResetEventSlim construction
+    //      ManualResetEventSlim.Wait()
+    //      ManualResetEventSlim.Set()
+    //      ManualResetEventSlim.Reset()
+    //      ManualResetEventSlim.IsSet
+    static void MRES_SetWaitReset()
+    {
+        ManualResetEventSlim mres1 = new ManualResetEventSlim(false); // initialize as unsignaled
+        ManualResetEventSlim mres2 = new ManualResetEventSlim(false); // initialize as unsignaled
+        ManualResetEventSlim mres3 = new ManualResetEventSlim(true);  // initialize as signaled
+
+        // Start an asynchronous Task that manipulates mres3 and mres2
+        var observer = Task.Factory.StartNew(() =>
         {
-            var t1 = new Thread(() => TravelThroughGates("Thread1", 5));
-            var t2 = new Thread(() => TravelThroughGates("Thread2", 6));
-            var t3 = new Thread(() => TravelThroughGates("Thread3", 12));
-            t1.Start();
-            t2.Start();
-            t3.Start();
-            Thread.Sleep(TimeSpan.FromSeconds(1));
-            Console.WriteLine("The gates are now open!");
-            _mainEvent.Set();
-            Thread.Sleep(TimeSpan.FromSeconds(2));
-            _mainEvent.Reset();
-            Console.WriteLine("The gates have been closed!");
-            Thread.Sleep(TimeSpan.FromSeconds(10));
-            Console.WriteLine("The gates are now open for the second time!");
-            _mainEvent.Set();
-            Thread.Sleep(TimeSpan.FromSeconds(2));
-            Console.WriteLine("The gates have been closed!");
-            _mainEvent.Reset();
+            mres1.Wait();
+            Console.WriteLine("observer sees signaled mres1!");
+            Console.WriteLine("observer resetting mres3...");
+            mres3.Reset(); // should switch to unsignaled
+            Console.WriteLine("observer signalling mres2");
+            mres2.Set();
+        });
 
-            Console.Write("begin cloase program");
-            Console.ReadKey();
-        }
+        Console.WriteLine("main thread: mres3.IsSet = {0} (should be true)", mres3.IsSet);
+        Console.WriteLine("main thread signalling mres1");
+        mres1.Set(); // This will "kick off" the observer Task
+        mres2.Wait(); // This won't return until observer Task has finished resetting mres3
+        Console.WriteLine("main thread sees signaled mres2!");
+        Console.WriteLine("main thread: mres3.IsSet = {0} (should be false)", mres3.IsSet);
 
-        static ManualResetEventSlim _mainEvent = new ManualResetEventSlim(false);
+        // It's good form to Dispose() a ManualResetEventSlim when you're done with it
+        observer.Wait(); // make sure that this has fully completed
+        mres1.Dispose();
+        mres2.Dispose();
+        mres3.Dispose();
+    }
 
-        static void TravelThroughGates(string threadName, int seconds)
+    // Demonstrates:
+    //      ManualResetEventSlim construction w/ SpinCount
+    //      ManualResetEventSlim.WaitHandle
+    static void MRES_SpinCountWaitHandle()
+    {
+        // Construct a ManualResetEventSlim with a SpinCount of 1000
+        // Higher spincount => longer time the MRES will spin-wait before taking lock
+        ManualResetEventSlim mres1 = new ManualResetEventSlim(false, 1000);
+        ManualResetEventSlim mres2 = new ManualResetEventSlim(false, 1000);
+
+        Task bgTask = Task.Factory.StartNew(() =>
         {
-            Console.WriteLine("{0} beigin to sleep in function TravelThroughGates", threadName);
-            Thread.Sleep(TimeSpan.FromSeconds(seconds));
-            Console.WriteLine("{0} waits for the gates to open!", threadName);
-            _mainEvent.Wait();  //等待set信息，得到了才
-            Console.WriteLine("{0} enters the gates!", threadName);
-        }
+            // Just wait a little
+            Thread.Sleep(100);
+
+            // Now signal both MRESes
+            Console.WriteLine("Task signalling both MRESes");
+            mres1.Set();
+            mres2.Set();
+        });
+
+        // A common use of MRES.WaitHandle is to use MRES as a participant in 
+        // WaitHandle.WaitAll/WaitAny.  Note that accessing MRES.WaitHandle will
+        // result in the unconditional inflation of the underlying ManualResetEvent.
+        WaitHandle.WaitAll(new WaitHandle[] { mres1.WaitHandle, mres2.WaitHandle });
+        Console.WriteLine("WaitHandle.WaitAll(mres1.WaitHandle, mres2.WaitHandle) completed.");
+
+        // Clean up
+        bgTask.Wait();
+        mres1.Dispose();
+        mres2.Dispose();
     }
 }
