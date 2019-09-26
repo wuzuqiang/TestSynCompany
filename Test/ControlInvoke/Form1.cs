e27f018fc4aebfc768403483b7818384e8feac6b
wuzuqiang <zuqiang_wu@skyseaok.com>
2019-09-26 10:14:48 +0800

    使用ManualResetEventSlim操作多线程

---
+++ Test/ControlInvoke/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlInvoke
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //System.Threading.Thread.CurrentThread.Name = "UIThread";
+            //Thread th = new Thread(new ThreadStart(StartThread));
+            //th.Start();
+            StartThread();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public delegate void treeinvoke();
+        private void UpdateTreeView()
+        {
+            MessageBox.Show(System.Threading.Thread.CurrentThread.Name);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.CurrentThread.Name = "UIThread";
+            Thread th = new Thread(new ThreadStart(StartThread));
+            th.Start();
+        }
+        private void StartThread()
+        {
+            Thread.CurrentThread.Name = "Work Thread";
+            treeView1.BeginInvoke(new treeinvoke(UpdateTreeView));  //不要在类构造函数Form1中调用StartThread函数，不然会报错“在创建窗口句柄之前，不能在控件上调用 Invoke 或 BeginInvoke。”
+        }
+    }
+
+
+}
---
+++ Test/ManualResetEventSlim_Test1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManualResetEventSlim_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    class ProgramTest
+    {
+        public void Main1(string[] args)
+        {
+            var t1 = new Thread(() => TravelThroughGates("Thread1", 5));
+            var t2 = new Thread(() => TravelThroughGates("Thread2", 6));
+            var t3 = new Thread(() => TravelThroughGates("Thread3", 12));
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            Thread.Sleep(TimeSpan.FromSeconds(6));
+            Console.WriteLine("The gates are now open!");
+            _mainEvent.Set();
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            _mainEvent.Reset();
+            Console.WriteLine("The gates have been closed!");
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+            Console.WriteLine("The gates are now open for the second time!");
+            _mainEvent.Set();
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            Console.WriteLine("The gates have been closed!");
+            _mainEvent.Reset();
+
+            Console.ReadKey();
+        }
+
+        static ManualResetEventSlim _mainEvent = new ManualResetEventSlim(false);
+
+        static void TravelThroughGates(string threadName, int seconds)
+        {
+            Console.WriteLine("{0} false to sleep", threadName);
+            Thread.Sleep(TimeSpan.FromSeconds(seconds));
+            Console.WriteLine("{0} waits for the gates to open!", threadName);
+            _mainEvent.Wait();
+            Console.WriteLine("{0} enters the gates!", threadName);
+        }
+    }
+}
