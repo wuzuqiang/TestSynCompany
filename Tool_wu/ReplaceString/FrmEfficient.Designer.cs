f5753769a7e0e3cd3f18aef802faf18a849b0f5e
wuzuqiang <zuqiang_wu@skyseaok.com>
2019-12-03 23:39:26 +0800

    添加 高效查找粗劣模型

---
+++ Tool_wu/ReplaceString/FrmEfficient.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ReplaceString
+{
+    partial class FrmEfficient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "查找内容中相同的两个字符串";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FrmEfficient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "FrmEfficient";
+            this.Text = "FrmEfficient";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
