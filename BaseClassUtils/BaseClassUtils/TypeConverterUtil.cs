08a7daf95e10a87b5ed9c213a2f05218ae4f8c46
wuzuqiang <zuqiang_wu@skyseaok.com>
2020-01-08 22:35:09 +0800

    添加 基础 > 类型转换

---
+++ BaseClassUtils/BaseClassUtils/TypeConverterUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseClassUtils
+{
+    public class TypeConverterUtil
+    {
+        public static T GetParameterValueBy<T>(string value, T defaultValue)
+        {
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+    }
+}
