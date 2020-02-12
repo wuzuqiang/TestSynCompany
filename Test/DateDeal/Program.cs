9626cb8e53a4926a60b0926b04c8da956325d1b1
wuzuqiang <1045774869@qq.com>
2020-02-12 10:51:14 +0800

    添加 日期操作项目，方便以后查阅

---
+++ Test/DateDeal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateDeal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DateTime.Now.ToString()：" + DateTime.Now.ToString());
+            Console.WriteLine("DateTime.Now.ToString()：" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+
+            Console.WriteLine("DateTime.Now.ToLongDateString()：" + DateTime.Now.ToLongDateString());
+            Console.WriteLine("DateTime.Now.ToShortTimeString()：" + DateTime.Now.ToShortTimeString());
+            Console.WriteLine("DateTime.Now.ToShortDateString()：" + DateTime.Now.ToShortDateString());
+            Console.WriteLine("DateTime.Now.ToShortTimeString()：" + DateTime.Now.ToShortTimeString());
+            Console.WriteLine("DateTime.Now.ToUniversalTime()：" + DateTime.Now.ToUniversalTime());
+            Console.WriteLine("DateTime.Now.ToOADate()：" + DateTime.Now.ToOADate());
+            Console.WriteLine("DateTime.Now.ToLocalTime()：" + DateTime.Now.ToLocalTime());
+            Console.ReadLine();
+        }
+    }
+}
