ced68d8eb20a0492cc8aec4e50487b62c45c6cee
wuzuqiang <zuqiang_wu@skyseaok.com>
2020-03-04 17:42:54 +0800

    调整 > WebService的Http SOAP方法测试

--- WebService/Client_ConsoleApp1/CallWebService.cs
+++ WebService/Client_ConsoleApp1/CallWebService.cs
@@ -15,24 +15,69 @@ namespace Client_ConsoleApp1
         {
             mUrl = url;
         }
-
-        /// <summary>
-        /// 调用接口
-        /// </summary>
-        /// <param name="methodName">方法名称</param>
-        /// <param name="param">参数名称</param>
-        /// <returns>返回值</returns>
-        public string callWebService(string methodName, Dictionary<string, string> param)
+        
+        
+        /// <summary>
+        /// 使用SOAP 1.1访问WebService
+        /// </summary>
+        /// <param name="host">主机IP，可为localhost</param>
+        /// <param name="methodName"></param>
+        /// <param name="strReqXml"></param>
+        /// <returns></returns>
+        public string callWebServiceBySOAP11(string host, string methodName, string strReqXml)
         {
             ///获取请求数据
-            byte[] data = getRequestData(methodName, param);  //System.Net.WebException:“远程服务器返回错误: (500) 内部服务器错误。”
-            //byte[] data = getRequestDataALL(methodName, param); //调用服务端的服务名称
-            //byte[] data = getBaseRequestData(methodName, param); //ERROR：System.Net.WebException:“远程服务器返回错误: (400) 错误的请求。”
+            StringBuilder requestData = new StringBuilder();
+            requestData.Append("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            requestData.Append("<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soapenc=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:tns=\"http://tempuri.org/\" xmlns:types=\"http://tempuri.org/encodedTypes\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">");
+            requestData.Append("  <soap:Body soap:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">");
+            requestData.Append("    <tns:JBSC_SCGL_GDQX>");
+            requestData.Append("      <reqXml xsi:type=\"xsd:string\">" + strReqXml + "</reqXml>");
+            requestData.Append("    </tns:JBSC_SCGL_GDQX>");
+            requestData.Append("  </soap:Body>");
+            requestData.Append("</soap:Envelope>");
+            
+            byte[] data = Encoding.UTF8.GetBytes(requestData.ToString());
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(mUrl);
             request.Method = "POST";
+            request.Host = host;
             request.ContentType = "text/xml; charset=utf-8";
             string mSoapAction = "http://tempuri.org/" + methodName;
             request.Headers.Add("SOAPAction", mSoapAction);
+            //request.ProtocolVersion = HttpVersion.Version11;
+            //request.Credentials = CredentialCache.DefaultCredentials;
+            request.ContentLength = data.Length;
+            Stream rStream = request.GetRequestStream();
+            rStream.Write(data, 0, data.Length);
+            rStream.Close();
+
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string result = reader.ReadToEnd();
+
+            dataStream.Close();
+            response.Close();
+            return result;
+        }
+        public string callWebServiceBySOAP12(string host, string methodName, string strReqXml)
+        {
+            ///获取请求数据
+            StringBuilder requestData = new StringBuilder();
+            requestData.Append("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            requestData.Append("<soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soapenc=\"http://www.w3.org/2003/05/soap-encoding\" xmlns:tns=\"http://tempuri.org/\" xmlns:types=\"http://tempuri.org/encodedTypes\" xmlns:rpc=\"http://www.w3.org/2003/05/soap-rpc\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\">");
+            requestData.Append("  <soap12:Body soap12:encodingStyle=\"http://www.w3.org/2003/05/soap-encoding\">");
+            requestData.Append("    <tns:JBSC_SCGL_GDQX>");
+            requestData.Append("      <reqXml xsi:type=\"xsd:string\">" + strReqXml + "</reqXml>");
+            requestData.Append("    </tns:JBSC_SCGL_GDQX>");
+            requestData.Append("  </soap12:Body>");
+            requestData.Append("</soap12:Envelope>");
+            
+            byte[] data = Encoding.UTF8.GetBytes(requestData.ToString());
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(mUrl);
+            request.Method = "POST";
+            request.Host = host;
+            request.ContentType = "application/soap+xml; charset=utf-8";
             request.ContentLength = data.Length;
             Stream rStream = request.GetRequestStream();
             rStream.Write(data, 0, data.Length);
@@ -48,132 +93,85 @@ namespace Client_ConsoleApp1
             return result;
         }
 
+        #region //网页上http://localhost:8085/MesToFLKService.asmx?op=JBSC_SCGL_GDQX上显示的内容
+        //        #region //网页上http://localhost:8085/MesToFLKService.asmx?op=JBSC_SCGL_GDQX上显示的内容
+        //        SOAP 1.1
+        //以下是 SOAP 1.2 请求和响应示例。所显示的占位符需替换为实际值。
 
-        /// <summary>
-        /// 获取请求内容(方法1) 适合 .Net
-        /// </summary>
-        /// <param name="methodName">方法名称</param>
-        /// <param name="param">参数</param>
-        /// <returns></returns>
-        public byte[] getRequestData(string methodName, Dictionary<string, string> param)
-        {
-            //http://localhost:9090/WebService1.asmx?op=
-            #region 以下是 SOAP 1.2 请求和响应示例。所显示的占位符需替换为实际值。HelloWorld()
-            /*
+        //POST /MesToFLKService.asmx HTTP/1.1
+        //Host: localhost
+        //Content-Type: text/xml; charset=utf-8
+        //Content-Length: length
+        //SOAPAction: "http://tempuri.org/transManufacturingFormula"
 
-             SOAP 1.1
-以下是 SOAP 1.2 请求和响应示例。所显示的占位符需替换为实际值。
+        //<?xml version = "1.0" encoding="utf-8"?>
+        //<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:tns="http://tempuri.org/" xmlns:types="http://tempuri.org/encodedTypes" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+        //  <soap:Body soap:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+        //    <tns:JBSC_SCGL_GDQX>
+        //      <reqXml xsi:type="xsd:string">string</reqXml>
+        //    </tns:JBSC_SCGL_GDQX>
+        //  </soap:Body>
+        //</soap:Envelope>
+        //HTTP/1.1 200 OK
+        //Content-Type: text/xml; charset=utf-8
+        //Content-Length: length
 
-POST /WebService1.asmx HTTP/1.1
-Host: localhost
-Content-Type: text/xml; charset=utf-8
-Content-Length: length
-SOAPAction: "http://tempuri.org/HelloWorld"
+        //<?xml version="1.0" encoding="utf-8"?>
+        //<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:tns="http://tempuri.org/" xmlns:types="http://tempuri.org/encodedTypes" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+        //  <soap:Body soap:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+        //    <tns:JBSC_SCGL_GDQXResponse>
+        //      <JBSC_SCGL_GDQXResult xsi:type="xsd:string">string</JBSC_SCGL_GDQXResult>
+        //    </tns:JBSC_SCGL_GDQXResponse>
+        //  </soap:Body>
+        //</soap:Envelope>
+        //SOAP 1.2
+        //以下是 SOAP 1.2 请求和响应示例。所显示的占位符需替换为实际值。
 
-<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-  <soap:Body>
-    <HelloWorld xmlns="http://tempuri.org/" />
-  </soap:Body>
-</soap:Envelope>
-HTTP/1.1 200 OK
-Content-Type: text/xml; charset=utf-8
-Content-Length: length
+        //POST /MesToFLKService.asmx HTTP/1.1
+        //Host: localhost
+        //Content-Type: application/soap+xml; charset=utf-8
+        //Content-Length: length
 
-<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-  <soap:Body>
-    <HelloWorldResponse xmlns="http://tempuri.org/">
-      <HelloWorldResult>string</HelloWorldResult>
-    </HelloWorldResponse>
-  </soap:Body>
-</soap:Envelope>
-             */
-            #endregion
-            #region 以下是 SOAP 1.2 请求和响应示例。所显示的占位符需替换为实际值。SayHello(string name)
-            /*
-SOAP 1.1
-以下是 SOAP 1.2 请求和响应示例。所显示的占位符需替换为实际值。
+        //<?xml version="1.0" encoding="utf-8"?>
+        //<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenc="http://www.w3.org/2003/05/soap-encoding" xmlns:tns="http://tempuri.org/" xmlns:types="http://tempuri.org/encodedTypes" xmlns:rpc="http://www.w3.org/2003/05/soap-rpc" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+        //  <soap12:Body soap12:encodingStyle="http://www.w3.org/2003/05/soap-encoding">
+        //    <tns:JBSC_SCGL_GDQX>
+        //      <reqXml xsi:type="xsd:string">string</reqXml>
+        //    </tns:JBSC_SCGL_GDQX>
+        //  </soap12:Body>
+        //</soap12:Envelope>
+        //HTTP/1.1 200 OK
+        //Content-Type: application/soap+xml; charset=utf-8
+        //Content-Length: length
 
-POST /WebService1.asmx HTTP/1.1
-Host: localhost
-Content-Type: text/xml; charset=utf-8
-Content-Length: length
-SOAPAction: "http://tempuri.org/SayHello"
+        //<?xml version="1.0" encoding="utf-8"?>
+        //<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenc="http://www.w3.org/2003/05/soap-encoding" xmlns:tns="http://tempuri.org/" xmlns:types="http://tempuri.org/encodedTypes" xmlns:rpc="http://www.w3.org/2003/05/soap-rpc" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+        //  <soap12:Body soap12:encodingStyle="http://www.w3.org/2003/05/soap-encoding">
+        //    <tns:JBSC_SCGL_GDQXResponse>
+        //      <rpc:result xmlns = "" > JBSC_SCGL_GDQXResult </ rpc:result>
+        //      <JBSC_SCGL_GDQXResult xsi:type="xsd:string">string</JBSC_SCGL_GDQXResult>
+        //    </tns:JBSC_SCGL_GDQXResponse>
+        //  </soap12:Body>
+        //</soap12:Envelope>
+        //HTTP POST
+        //以下是 HTTP POST 请求和响应示例。所显示的占位符需替换为实际值。
 
-<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-  <soap:Body>
-    <SayHello xmlns="http://tempuri.org/">
-      <name>string</name>
-    </SayHello>
-  </soap:Body>
-</soap:Envelope>
-HTTP/1.1 200 OK
-Content-Type: text/xml; charset=utf-8
-Content-Length: length
+        //POST /MesToFLKService.asmx/JBSC_SCGL_GDQX HTTP/1.1
+        //Host: localhost
+        //Content-Type: application/x-www-form-urlencoded
+        //Content-Length: length
 
-<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-  <soap:Body>
-    <SayHelloResponse xmlns="http://tempuri.org/">
-      <SayHelloResult>string</SayHelloResult>
-    </SayHelloResponse>
-  </soap:Body>
-</soap:Envelope>
-             */
-            #endregion
-            #region 以下是 HTTP POST 请求和响应示例。所显示的占位符需替换为实际值。
-            /*
-             以下是 HTTP POST 请求和响应示例。所显示的占位符需替换为实际值。
+        //reqXml = string
+        //HTTP/1.1 200 OK
+        //Content-Type: text/xml; charset=utf-8
+        //Content-Length: length
 
-POST /WebService1.asmx/SayHello HTTP/1.1
-Host: localhost
-Content-Type: application/x-www-form-urlencoded
-Content-Length: length
+        //<?xml version="1.0" encoding="utf-8"?>
+        //<string xmlns = "http://tempuri.org/" > string </ string >
+        //        #endregion
+        #endregion
 
-name=string
-HTTP/1.1 200 OK
-Content-Type: text/xml; charset=utf-8
-Content-Length: length
-
-<?xml version="1.0" encoding="utf-8"?>
-<string xmlns="http://tempuri.org/">string</string>
-             */
-            #endregion
-            StringBuilder requestData = new StringBuilder("<?xml version=\"1.0\" encoding=\"utf-8\"?>")
-              .Append("<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">")
-              .Append("  <soap:Body>")
-              .Append("<").Append(methodName)
-              .Append(" xmlns=\"http://tempuri.org/\">");
-            foreach (KeyValuePair<string, string> item in param)
-            {
-                requestData.Append("<").Append(item.Key).Append(">")
-                .Append(item.Value)
-                .Append("</").Append(item.Key).Append(">");
-            }
-            requestData.Append("</").Append(methodName).Append(">")
-            .Append("</soap:Body>")
-            .Append("</soap:Envelope>");
-            string val = requestData.ToString();
-            byte[] data = Encoding.UTF8.GetBytes(val);
-            return data;
-        }
-
-
-        public byte[] getBaseRequestData(string methodName, Dictionary<string, string> param)
-        {
-            StringBuilder requestBuider = new StringBuilder();
-            string val = requestBuider.ToString();
-            byte[] data = Encoding.UTF8.GetBytes(val);
-            foreach (KeyValuePair<string, string> item in param)
-            {
-                requestBuider.Append(item.Key);
-                requestBuider.Append(item.Value);
-            }
-            return data;
-        }
-
+        #region 原先网络上的方法
         /// <summary>
         /// 获取数据(方法2) 兼容所有的(java soap 服务端和.net  soap 服务端)
         /// </summary>
@@ -200,5 +198,7 @@ Content-Length: length
             byte[] data = Encoding.UTF8.GetBytes(val);
             return data;
         }
+        #endregion
+
     }
 }
--- WebService/Client_ConsoleApp1/Program.cs
+++ WebService/Client_ConsoleApp1/Program.cs
@@ -10,17 +10,47 @@ namespace Client_ConsoleApp1
     {
         static void Main(string[] args)
         {
+            try
+            {
+                TestWebserviceBySoap11();
+                Console.WriteLine("----------------------------------------");
+                TestWebserviceBySoap12();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("------->出现错误：" + ex.Message);
+            }
+
+            Console.Write("按回车键退出！");
+            Console.ReadLine();
+        }
+
+
+        public static void TestWebserviceBySoap11()
+        {
+            //直接通过Http访问WebService服务
+            WebServiceCall webServiceCall = new WebServiceCall("http://localhost:8085/MesToFLKService.asmx");
+            //WebServiceCall webServiceCall = new WebServiceCall("localhost:8085"); //ERROR:无法识别该 URI 前缀。
+            //WebServiceCall webServiceCall = new WebServiceCall("http://localhost:8085"); //ERROR:好像得到一个完整的HTML，但是看着数据很不对，具体还待分析
+            //WebServiceCall webServiceCall = new WebServiceCall("http://localhost:8085/MesToFLKService.asmx?op=JBSC_SCGL_GDQX");
+            var ret = webServiceCall.callWebServiceBySOAP11("localhost", "transManufacturingFormula", "test");
+            Console.WriteLine(ret);
+        }
+
+        public static void TestWebserviceBySoap12()
+        {
+            //WebServiceCall webServiceCall = new WebServiceCall("http://localhost:8085/MesToFLKService.asmx?op=JBSC_SCGL_GDQX");
+            WebServiceCall webServiceCall = new WebServiceCall("http://localhost:8085/MesToFLKService.asmx");
+            var ret = webServiceCall.callWebServiceBySOAP12("localhost", "transManufacturingFormula", "test");
+            Console.WriteLine(ret);
+        }
+
+
+        public static void Test00()
+        {
             //直接使用引用服务的方式来调用
             ServiceReference1.WebService1SoapClient webService = new ServiceReference1.WebService1SoapClient();
             Console.WriteLine(webService.SayHello("wu zu qiang"));
-
-            //直接通过Http访问WebService服务
-            WebServiceCall webServiceCall = new WebServiceCall("http://localhost:9090/WebService1.asmx");
-            var dicTemp = new Dictionary<string, string>();
-            dicTemp.Add("name", "wuzuqiang");
-            var ret = webServiceCall.callWebService("SayHello", dicTemp);
-
-            Console.ReadLine();
         }
     }
 }
