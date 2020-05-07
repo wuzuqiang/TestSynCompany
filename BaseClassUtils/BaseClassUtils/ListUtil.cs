7fd6ffdb4defa2c4391eae5751719ea797e87e72
wuzuqiang <1045774869@qq.com>
2020-05-08 01:06:48 +0800

    添加 工具类list，使用了泛型，委托降序排列

---
+++ BaseClassUtils/BaseClassUtils/ListUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseClassUtils
+{
+    public static class ListUtil
+    {
+        /// <summary>
+        /// 将List中的内容以int值类型降序处理
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="listT"></param>
+        /// <returns></returns>
+        public static List<T> SortByDesc<T>(this List<T> listT) where T : struct
+        {
+            listT.Sort((h1, h2) =>
+            {
+                if (Convert.ToInt32(h1) < Convert.ToInt32(h2)) return 1; return -1; //左值小于右值，返回-1，为升序，如果返回1，就是降序
+            });
+            return listT;
+        }
+
+    }
+}
