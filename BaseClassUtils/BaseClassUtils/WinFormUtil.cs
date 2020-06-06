8d5cc28148468809528b9657aafc003ca2ed8f34
wuzuqiang <1045774869@qq.com>
2020-06-06 16:31:34 +0800

    添加 封装Winform弹窗选择目录和选择文件功能

---
+++ BaseClassUtils/BaseClassUtils/WinFormUtil.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaseClassUtils
+{
+    public class WinFormUtil
+    {
+        public static string ShowOpenFileDialog()
+        {
+            string result = "";
+            OpenFileDialog dlg = new OpenFileDialog();
+            if(dlg.ShowDialog() == DialogResult.OK)
+            {
+                result = dlg.FileName;
+            }
+            return result;
+        }
+
+        public static string ShowFolderBrowserDialog()
+        {
+            string result = "";
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                result = dlg.SelectedPath;
+            }
+            return result;
+        }
+    }
+}
