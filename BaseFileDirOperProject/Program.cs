5c261329912f63c5cd4c2a962de3ee585266d9f2
wuzuqiang <1045774869@qq.com>
2020-06-24 15:16:49 +0800

    测试 调整的源码

--- BaseFileDirOperProject/Program.cs
+++ BaseFileDirOperProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BaseClassUtils.SimuLinq;
 
 namespace BaseFileDirOperProject
 {
@@ -14,6 +15,12 @@ namespace BaseFileDirOperProject
         [STAThread]
         static void Main()
         {
+            List<String> listStr = new List<string>() { "1", "2", "3" };
+            foreach(var source in listStr.MyWhere(w => w=="2"))
+            {
+                var temp = source;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
