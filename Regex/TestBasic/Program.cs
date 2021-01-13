0254d08e3ba0e04d852715c5ed4f04a41fa59a9f
wuzuqiang <1045774869@qq.com>
2021-01-13 17:20:17 +0800

    新增 测试基本功能项目，这次是测试数组的连接显示

---
+++ Regex/TestBasic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBasic
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] array = new string[] { "1", "2", "one", "two"};
+			Console.WriteLine(string.Join("、", array));
+			Console.WriteLine(string.Join("、", new string[] { }));
+			Console.WriteLine(string.Join("、", new string[] { "1"}));
+
+			Console.ReadLine();
+		}
+	}
+}
