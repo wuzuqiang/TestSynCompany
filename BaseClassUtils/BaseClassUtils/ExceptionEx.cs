ffb4602ab32ea78e3cbdeb7356c919dda0d65da4
wuzuqiang <1045774869@qq.com>
2021-01-30 13:25:01 +0800

    添加 Exception类

---
+++ BaseClassUtils/BaseClassUtils/ExceptionEx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseClassUtils
+{
+	public class ExceptionEx : Exception
+	{
+		public ExceptionEx() : base()
+		{
+			this.MessageEx = base.Message + "test";
+		}
+		public ExceptionEx(string message) : base(message)
+		{
+			this.MessageEx = message + "testEx";
+		}
+
+		public string MessageEx { get; set; }
+	}
+}
