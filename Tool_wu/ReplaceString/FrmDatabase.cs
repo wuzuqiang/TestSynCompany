a977d1c9227600db4534bc5cfe6ddd4739253c01
wuzuqiang <1045774869@qq.com>
2021-01-30 13:25:51 +0800

    添加 Database数据库操作窗体

---
+++ Tool_wu/ReplaceString/FrmDatabase.cs
@@ -0,0 +1,40 @@
+﻿using BaseClassUtils;
+using ReplaceString.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReplaceString
+{
+	public partial class FrmDatabase : Form
+	{
+		public FrmDatabase()
+		{
+			InitializeComponent();
+		}
+
+		private void FrmDatabase_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		public static void InsertDatabase(ReplaceHistoryModel replaceHistoryModel)
+		{
+
+			try
+			{
+				TransferHelper.ExecuteInsert(replaceHistoryModel);
+			}
+			catch (ExceptionEx ex)
+			{
+				MessageBox.Show(ex.MessageEx);
+			}
+		}
+	}
+}
