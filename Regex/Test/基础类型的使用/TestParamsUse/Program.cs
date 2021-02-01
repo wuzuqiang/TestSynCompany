5c687bcd5fd8e48593265b1ea81a8455a019876e
wuzuqiang <1045774869@qq.com>
2021-02-01 19:55:52 +0800

    添加 对Params型数据进行尝试

---
+++ Regex/Test/基础类型的使用/TestParamsUse/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestParamsUse
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array_a = ToArray(1, 2, 3, 4);
+			ShowData(array_a);
+
+			int[] array_b = ToArray(array_a);
+			ShowData(array_b);
+
+			Console.ReadLine();
+		}
+
+		public static int[] ToArray(params int[] arr)
+		{
+			return arr;
+		}
+
+		public static void ShowData(int[] arr)
+		{
+			foreach(var data in arr)
+			{
+				Console.Write($"{data}\t");
+			}
+			Console.WriteLine();
+		}
+	}
+}
