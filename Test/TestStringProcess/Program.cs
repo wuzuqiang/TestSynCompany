d0dd146018664ae11e36fcb9a84158bb12ed37f7
wuzuqiang <1045774869@qq.com>
2021-03-24 15:52:25 +0800

    测试 从中文中读出拼音首字母

---
+++ Test/TestStringProcess/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseClassUtils;
+
+namespace TestStringProcess
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(StringUtils.GetSpellCode("asx030609中国龙无敌的"));
+
+			Console.ReadLine();
+		}
+	}
+}
