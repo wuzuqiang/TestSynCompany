5d36bc25b067101b4adbec33c74d9228e23ba270
wuzuqiang <1045774869@qq.com>
2021-06-02 10:55:47 +0800

    添加 网络相关的测试

---
+++ Test/NetWorkConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace NetWorkConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var hostAddress = HttpContext.Current.Request.UserHostAddress;	//失败.Current.Request为null会导致异常
+			Console.WriteLine($"当前地址为：{hostAddress}");
+
+			Console.ReadLine();
+		}
+	}
+}
